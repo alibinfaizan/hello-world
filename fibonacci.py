fd2e072f7aa6a64ac5c726c74260f4be42b8a6a1
alibinfaizan <68111001+alibinfaizan@users.noreply.github.com>
2021-01-25 18:25:11 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,2 +1,13 @@
 def fibo(n):
-    
+    if n <=1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+nth = int(input('Enter a number : '))
+
+if nth<=0:
+    print('You typed a negtive integer.')
+else:
+    for i in range(nth+1):
+        print(fibo(i))
