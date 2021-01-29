8659e36be25a0bbcd3816cbfcb586f3c2bc67cb4
alibinfaizan <68111001+alibinfaizan@users.noreply.github.com>
2021-01-29 18:35:17 +0530

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,2 +1,11 @@
 def b_search(arr, key, low high):
-
+    if low > high:
+        return "Number Not Found!"
+    else:
+        mid = (low + high) // 2
+        if key==arr[mid]:
+            return "Number Found!"
+        elif key < arr[mid]:
+            return b_search(arr, key, low mid-1)
+        else:
+            return b_search(arr, key, mid+1, high)
